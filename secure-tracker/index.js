6574bfc8a785b30bee8d94229941e6f250726b82
Abdullah <alihamza@wearespark.me>
2026-09-21 18:55:15 +0500

    connecting DB and and modifiy it accordigly

---
+++ secure-tracker/index.js
@@ -0,0 +1,60 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js"
+import { getDatabase, 
+        ref,
+        push,
+        onValue  } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-database.js"
+
+
+const firebaseConfig = {
+    databaseURL: import.meta.env.VITE_DATABASE_URL
+}
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app)
+const referenceInDB = ref(database, "leads") 
+
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deletBtn = document.getElementById("delet-btn")
+const saveTab = document.getElementById("save-tab")
+
+onValue(referenceInDB, function(snapshot){
+    const snapShotVale = snapshot.val()
+    const leads = Object.values(snapShotVale)
+    renderLeads(leads)
+})
+ 
+function leadPusher(){
+    push(referenceInDB, inputEl.value)
+    inputEl.value = ""
+}
+
+function renderLeads(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
+}
+
+function deletAll(){
+   
+}
+
+function deletAllHandler(){
+    deletAll()
+    renderLeads()
+}
+
+// event lisiners
+
+deletBtn.addEventListener("dblclick", deletAllHandler)
+inputBtn.addEventListener("click", leadPusher)
+saveTab.addEventListener("click", SaveChormeTabs) 
